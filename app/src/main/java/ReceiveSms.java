6555467b914722e91f56ff7a67fa99b0684cbdc6
Kai Jun <1806357H@student.tp.edu.sg>
2019-10-12 14:31:15 +0800

    test

---
+++ app/src/main/java/ReceiveSms.java
@@ -0,0 +1,11 @@
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class ReceiveSms extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context,"SMS RECEIVED", Toast.LENGTH_LONG).show();
+    }
+}
